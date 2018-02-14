6a100e9911c045910865dfdd862d5f7f3d9ca71d
vachb8648 <vachb8648@FHC-IC0034099.wrdsb.ca>
2018-02-14 10:47:36 -0500

    Working Ghost Scratch Currently Missing:    - A.I. (Controls with spacebar)    - Hits or Walls

--- core/src/com/gdx/hamsters/Hamsters.java
+++ core/src/com/gdx/hamsters/Hamsters.java
@@ -1,14 +1,36 @@
 package com.gdx.hamsters;
 
 import com.badlogic.gdx.Game;
+import com.gdx.scratches.ScrSwitch1;
+import com.gdx.scratches.ScrSwitch2;
 import com.gdx.scratches.scraGhosts;
 
 public class Hamsters extends Game {
 
+    scraGhosts scraGhosts;
+    ScrSwitch1 ScrSwitch1;
+    ScrSwitch2 ScrSwitch2;
+    int nScreen;
+
+    public void updateState(int _nScreen) {
+        nScreen = _nScreen;
+        if (nScreen == 0) {
+            setScreen(ScrSwitch1);
+        } else if (nScreen == 1) {
+            setScreen(ScrSwitch2);
+        } else if (nScreen == 2) {
+            setScreen(scraGhosts);
+        }
+    }
+
     @Override
     public void create() {
-        this.setScreen(new scraGhosts(this));
-
+        nScreen = 2;
+        ScrSwitch1 = new ScrSwitch1(this);
+        ScrSwitch2 = new ScrSwitch2(this);
+        scraGhosts = new scraGhosts(this);
+        updateState(nScreen);
+        
     }
 
     @Override
